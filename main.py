9d29b5dfa9eae6dc0b83438b6c6b24a0d3c8884b
Rushi Chaudhari <rushic24@gmail.com>
2017-09-18 13:51:35 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import time 
+import pyautogui
+import numpy as np
+def get_image(im):
+    return im.crop((im.width/3,0,(2.151*im.width)/3,im.height))
+
+def action(key):
+    pyautogui.press(key)
+
+time.sleep(5)
+flag=0
+n=int(input("Enter pages:\n"))
+
+for i in range(6):
+    time.sleep(0.6)
+    im = pyautogui.screenshot()
+    c=get_image(im)
+    c.save(str(i)+".jpg")
+    time.sleep(0.15)
+    action("right")

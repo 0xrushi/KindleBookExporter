be789c6db323528336a95d6e869c7c8c964f018d
Rushi Chaudhari <6279035+0xrushi@users.noreply.github.com>
2023-10-28 09:42:23 -0500

    Update makepdf.py

--- makepdf.py
+++ makepdf.py
@@ -1,4 +1,11 @@
 import img2pdf
 import os
+
+jpg_files = [i for i in os.listdir('./') if i.endswith(".jpg")]
+
+# Sort the list in serial order by numeric value
+jpg_files.sort(key=lambda x: int(x.split('.')[0]))
+
+# print(jpg_files)
 with open("output.pdf", "wb") as f:
-    f.write(img2pdf.convert([i for i in os.listdir('./') if i.endswith(".jpg")]))
+    f.write(img2pdf.convert(jpg_files))
